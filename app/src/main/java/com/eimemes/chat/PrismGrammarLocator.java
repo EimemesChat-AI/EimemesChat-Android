adb0352c372a59a0289ef12f1e14edd4915adfc2
EimemesChat-AI <eimemeschatai@gmail.com>
2026-03-11 15:34:58 +0530

    Update PrismGrammarLocator.java

--- app/src/main/java/com/eimemes/chat/PrismGrammarLocator.java
+++ app/src/main/java/com/eimemes/chat/PrismGrammarLocator.java
@@ -4,11 +4,10 @@ import io.noties.prism4j.annotations.PrismBundle;
 
 @PrismBundle(
     include = {
-        "javascript", "python", "java", "kotlin", "cpp", "c",
-        "css", "json", "sql", "typescript",
-        "go", "swift", "php", "ruby", "rust", "clike"
+        "javascript", "python", "java", "kotlin", "c",
+        "css", "json", "sql", "go", "swift", "php", "ruby", "rust", "clike"
     },
-    grammarLocatorClassName = ".GrammarLocatorDef"
+    grammarLocatorClassName = "com.eimemes.chat.GrammarLocatorDef"
 )
 public class PrismGrammarLocator {
     // Annotation processor generates GrammarLocatorDef.java at build time
